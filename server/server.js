baab4b1977716515d771ac3bfbd1db110e87ee76
ovelha estoica <ovelhaestoica@bitbucket.org>
2020-03-23 13:25:26 -0300

    server listening /graph

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/graph', function(req, res) {
+    res.send({ graph: "grafoo" });   
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
