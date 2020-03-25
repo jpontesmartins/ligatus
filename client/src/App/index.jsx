c459f2094351f263c65adc3f379c29594c8a9274
ovelha estoica <ovelhaestoica@bitbucket.org>
2020-03-25 14:28:13 -0300

    Grafo component deleted

--- client/src/App/index.jsx
+++ client/src/App/index.jsx
@@ -1,11 +1,54 @@
 import React, { Component } from 'react';
-import Grafo from '../Grafo';
-import grafo from './grafo.json';
+import axios from "axios";
+import grafo2 from './grafo.json';
+import Graph from "react-graph-vis";
+import { events, options } from "./grafoConfig";
+
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      grafo: { nodes: [], edges: [] }
+    }
+  }
+
+  componentDidMount = () => {
+    axios
+      .get("http://localhost:5000/graph")
+      .then(response => {
+        this.setState({ grafo: response.data });
+      });
+  }
+
+  
+
   render() {
-    return (<Grafo grafo={grafo} />);
+    const { grafo, teste } = this.state;
+
+    const containerGraph = {
+        backgroundColor: "#FFF",
+        marginBottom: "10px",
+        width: "700px",
+        height: "500px",
+        marginTop: "25px",
+        marginLeft: "50px",
+        boxShadow: "0 4px 8px 0 #444"
+    }
+
+    return (
+      <div style={containerGraph}>
+        {teste}
+        <Graph
+          graph={grafo} 
+          options={options} 
+          events={events}
+          style={{ height: "100%" }}  />
+
+
+      </div>);
   }
 }
 
--- server/server.js
+++ server/server.js
@@ -2,14 +2,24 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
+const cors = require("cors");
+
+const serviceGraph = require("./domain/services");
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
-app.get('/graph', function(req, res) {
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
-    
-    res.send({ graph: "grafoo" });   
+app.get('/graph', function (req, res) {
+    const graph = serviceGraph.getGraph();
+    res.send(graph);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
