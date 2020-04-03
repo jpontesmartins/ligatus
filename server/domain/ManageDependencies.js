3ee150ea090a9b13c16e0335de431e937a0b46f4
ovelha estoica <ovelhaestoica@bitbucket.org>
2020-04-03 10:10:02 -0300

    movendo de lugara chamada do service

--- server/domain/ManageDependencies.js
+++ server/domain/ManageDependencies.js
@@ -4,12 +4,14 @@ const service = require("../services");
 const Dependency = require("./Dependency");
 
 class ManageDependencies {
-    constructor(file) {
+    constructor(file, service) {
         this.file = file;
         this.localDependencies = [];
         this.allDependencies = new Set();
         this.locals = new Set();
         this.nodes;
+
+        this.service = service;
     }
 
     getLocalDependencies = async () => {
@@ -29,7 +31,7 @@ class ManageDependencies {
     }
 
     getDependenciesFromFile = async (file) => {
-        const dependencies = await service.getDependencies(file);
+        const dependencies = await this.service.getDependencies(file);
         dependencies.map(d => {
             if (this.isLocalFile(d)) {
                 this.localDependencies.push(d);
--- server/gateways/index.js
+++ server/gateways/index.js
@@ -1,8 +1,10 @@
 const ManageDependencies = require("../domain/ManageDependencies");
 const ManageGraph = require("../domain/ManageGraph");
 
+const service = require("../services");
+
 getGraph = async (file) => {
-    const manageDependencies = new ManageDependencies(file);
+    const manageDependencies = new ManageDependencies(file, service);
     const manageGraph = new ManageGraph(manageDependencies);
     const graph = manageGraph.create();
     return graph;
