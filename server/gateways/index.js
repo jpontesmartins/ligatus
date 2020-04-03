7ecd8111961826e16e00cb4b95f1d4b40b2937fd
ovelha estoica <ovelhaestoica@bitbucket.org>
2020-04-03 09:19:38 -0300

    ManageGraph working..

--- server/gateways/index.js
+++ server/gateways/index.js
@@ -9,49 +9,9 @@ getGraph = async (file) => {
     const local = await manageDependencies.getLocalDependencies();
     const all = manageDependencies.getAllDependencies();
 
-    let graph = getGraphhh(file, all, local);
+    const manageGraph = new ManageGraph(file, all, local);
+    let graph = manageGraph.create();
 
-    return graph;
-}
-
-function getGraphhh(file, all, local) {
-    const nodes = new Map();
-    nodes.set(file, 1);
-    let nodeKey = 2;
-    all.forEach(dep => {
-        nodes.set(dep, nodeKey);
-        nodeKey++;
-    });
-    let graphNodes = [];
-    nodes.forEach((nodeId, key) => {
-        const graphNode = {
-            id: nodeId,
-            label: key,
-            shape: "image",
-            image: "https://cdn.discordapp.com/attachments/253635650870444032/650688296183136270/unknown.png"
-        };
-        graphNodes.push(graphNode);
-    });
-    let graph = {};
-    let edges = [];
-    local.forEach(localDep => {
-        const from = nodes.get(localDep.file);
-        const to = [];
-        localDep.dependencies.map(dependency => {
-            to.push(dependency);
-        });
-        to.map(dep => {
-            const edge = {
-                from: from,
-                to: nodes.get(dep)
-            };
-            edges.push(edge);
-        });
-        graph = {
-            nodes: graphNodes,
-            edges: edges
-        };
-    });
     return graph;
 }
 
